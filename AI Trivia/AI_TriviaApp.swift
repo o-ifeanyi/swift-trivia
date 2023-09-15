d5e70a09bcb3a07fea4ffb581861e99f5c960fee
o-ifeanyi <onuifeanyi95@gmail.com>
2023-09-15 12:23:24 +0100

    Initial Commit

---
+++ AI Trivia/AI_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_TriviaApp.swift
+//  AI Trivia
+//
+//  Created by Ifeanyi Onuoha on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AI_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
